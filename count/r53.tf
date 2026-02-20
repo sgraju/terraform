96c2a2e4b6f8664b67d74c0b05f7c7440f72e7a3
sgraju <sgraju.tibco@gmail.com>
2026-02-20 12:51:45 +0400

    terraform

--- count/r53.tf
+++ count/r53.tf
@@ -7,3 +7,13 @@ resource "aws_route53_record" "roboshop" {
   records = [aws_instance.terraform[count.index].private_ip]
   allow_overwrite = true
 }
+
+# resource "aws_route53_record" "frontend" {
+#   count = 1
+#   zone_id = var.zone_id
+#   name = "${var.instances[count.index]}.${var.domain_name}"
+#   type = "A"
+#   ttl = 1
+#   records = [aws_instance.terraform[count.index].private_ip]
+#   allow_overwrite = true
+# }
---
+++ for-loop/variables.tf
@@ -0,0 +1,16 @@
+variable "instances" {
+  # default = ["mongodb","mysql","rabbitmq","redis","catalogue","user","cart","shipping","payment","frontend"]
+    default = {
+      mongodb = "t3.micro"
+      redis = "t3.micro"
+      mysql = "t3.small"
+    }
+}
+
+variable "zone_id" {
+  default = "Z0732355102QE6GB8XDYY"
+}
+
+variable "domain_name" {
+  default = "sgrdevsecops.fun"
+}
