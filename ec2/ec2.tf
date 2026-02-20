1b27b81ce8a39c33645cca13efde6e08016b3c27
sgraju <sgraju.tibco@gmail.com>
2026-02-20 13:19:57 +0400

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "raju" {
+resource "aws_instance" "terraform" {
   ami = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
   tags = {
-    Name = "raju"
+    Name = "terraform"
     terraform = "true"
   }
 }
