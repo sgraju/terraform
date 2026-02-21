1a6b7694ade92cc2ae739002263108367c0c5d56
sgraju <sgraju.tibco@gmail.com>
2026-02-21 08:20:32 +0400

    terraform

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "terraform" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
   tags = {
-    Name = "terraform"
+    Name = "terraform-1"
     terraform = "true"
   }
 
@@ -16,6 +16,29 @@ resource "aws_instance" "terraform" {
   command = "echo Instance is destroyed"
   when = destroy
 }
+
+connection {
+  type = "ssh"
+  user = "ec2-user"
+  password = "DevOps321"
+  host = self.public_ip
+}
+
+provisioner "remote-exec" {
+  inline = [
+    "sudo dnf install nginx -y",
+    "sudo systemctl start nginx"
+  ]
+}
+
+provisioner "remote-exec" {
+  inline = [
+    "sudo systemctl stop nginx",
+    "echo 'successfully stopped nginx server' "
+  ]
+  when = destroy
+}
+
 }
 
 resource "aws_security_group" "allow_all" {
